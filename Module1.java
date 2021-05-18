d7035456efb4bb5a40866c9c30fa9c2b68eca43c
Raj Dedhia <rajdedhia10@gmail.com>
2021-05-19 01:48:45 +0530

    push test 2

--- Module1.java
+++ Module1.java
@@ -1,5 +1,5 @@
 class Module1 {
     public static void main(String[] args) {
-        System.out.println("Module 1 done! Test for push please work");
+        System.out.println("Module 1 done! Test for push");
     }
 }
